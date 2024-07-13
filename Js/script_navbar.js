90a3e677b4ddad33fc3fdaeb51569c381d15ba1b
Vicente <vvenancio06@gmail.com>
2024-07-12 22:10:58 -0300

    NavBar finalizada (sem Tailwind)

---
+++ Js/script_navbar.js
@@ -0,0 +1,4 @@
+let perfilDropDownList = document.querySelector(".perfil-dropdown-list");
+let btn = document.querySelector("#perfil-dropdown-btn");
+
+const toggle = () => perfilDropDownList.classList.toggle("active");
