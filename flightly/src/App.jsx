1335d7cbcd427e5c711b191dc8aca9274ac64e17
Vicente <vvenancio06@gmail.com>
2024-07-21 22:49:59 -0300

    NavBar funcionando Totalmente

--- flightly/src/App.jsx
+++ flightly/src/App.jsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './Pages/Home_Voo';
+import HospedagemPage from './Pages/HospedagemPage';
+import CarroPage from './Pages/CarroPage';
+import PontoTuristicoPage from './Pages/PontoTuristicoPage';
+
 
 
 export default function App() {
   return (
-    <Home/>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/Hospedagem" element={<HospedagemPage/>}/>
+          <Route path="/Carros" element={<CarroPage/>}/>
+          <Route path="/PontosTuristicos" element={<PontoTuristicoPage/>}/>
+        </Routes>
+      </Router>
+    </>
   );
 }
 
---
+++ flightly/src/Pages/PontoTuristicoPage.jsx
@@ -0,0 +1,18 @@
+import NavBar from "../Componentes/NavBar/NavBar";
+export default function Carro_page (){
+    return (
+        <>
+            <NavBar/>
+            <main>
+                klkdokode
+                d9okedk
+                odkeokedokedokdeo <br />
+                odkokedediiedjijde <br />
+                jdjedjideje <br />
+                oi
+                
+
+            </main>
+        </>
+    )
+}
