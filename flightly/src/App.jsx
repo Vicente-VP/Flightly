77cde1db2c0336f1ca82a053193fc17edd2700b2
Breno Saraiva <brenosaraiva27s@gmail.com>
2024-07-23 13:14:32 -0300

    Home Voos transformada para react

--- flightly/src/App.jsx
+++ flightly/src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import Home from './Pages/Home_Voo';
+import Home from './Pages/Home_Voo/Home_Voo';
 import HospedagemPage from './Pages/HospedagemPage';
 import CarroPage from './Pages/CarroPage';
 import PontoTuristicoPage from './Pages/PontoTuristicoPage';
--- flightly/src/Pages/Home_Voo.jsx
+++
@@ -1,9 +0,0 @@
-import NavBar from "../Componentes/NavBar/NavBar";
-
-export default function Home (){
-    return (
-        <>
-            <div style={{height: 76+'px'}}><NavBar/></div>
-        </>
-    )
-}
