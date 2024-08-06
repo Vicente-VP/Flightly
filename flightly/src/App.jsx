ff5b519cc9a14d033ad0afc4591087cfe6912c6f
AGATA CECILIA <ceciliacesquim@gmail.com>
2024-08-06 20:29:43 -0300

    Telas login e cadastro

--- flightly/src/App.jsx
+++ flightly/src/App.jsx
@@ -4,8 +4,8 @@ import Home from './Pages/Home_Voo';
 import HospedagemPage from './Pages/HospedagemPage';
 import CarroPage from './Pages/CarroPage';
 import PontoTuristicoPage from './Pages/PontoTuristicoPage';
-
-
+import Cadastro from './Pages/Cadastro';
+import Login from './Pages/Login';
 
 export default function App() {
   return (
@@ -16,6 +16,8 @@ export default function App() {
           <Route path="/Hospedagem" element={<HospedagemPage/>}/>
           <Route path="/Carros" element={<CarroPage/>}/>
           <Route path="/PontosTuristicos" element={<PontoTuristicoPage/>}/>
+          <Route path="/Cadastro" element={<Cadastro/>}/>
+          <Route path="/Login" element={<Login/>}/>
         </Routes>
       </Router>
     </>
