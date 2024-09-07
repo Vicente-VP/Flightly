22106f94827a79ecf9f4477579df147e98c50ee1
ViniciusCrozato <160658562+ViniciusCrozato@users.noreply.github.com>
2024-09-06 23:50:08 -0300

    Card Informações 95% Finalizado

--- flightly/src/Componentes/Card_Informacoes/Carros/CardInfoCarros.jsx
+++ flightly/src/Componentes/Card_Informacoes/Carros/CardInfoCarros.jsx
@@ -11,46 +11,46 @@ import large_bag from '../../../Images/Info-Car-Icon/travelling-bag.png';
 export default function InfoCarros(){
     return(
         <>
-    <div class="card-horizontal">
-        <div class="card-image">
-            <img src={ferrari} className='card-image-car'/>
-            <img src={movida} className='card-image-locadora'/>
-        </div>
-        <div class="card-content">
-            <div className='title-car'>
-                <label htmlFor="carro" className='lbl-carro'>Fiat Mobi</label>
+            <div class="card-horizontal">
+                <div class="card-image">
+                    <img src={ferrari} className='card-image-car'/>
+                    <img src={movida} className='card-image-locadora'/>
+                </div>
+                <div class="card-content">
+                    <div className='title-car'>
+                        <label htmlFor="carro" className='lbl-carro'>Fiat Mobi</label>
+                    </div>
+                    <div className='sub-car'>
+                        <label htmlFor="local" className='lbl-local'>São Paulo - Juscelino Kubitscheck</label>
+                    </div>
+                    <div className='info-left'>
+                        <img src={people} className='icon-assento' /> 
+                        <label htmlFor="number-assento" className='lbl-assento'> 5</label><label htmlFor="assento" className='lbl-assento'> Assentos</label>
+                        <br />
+                        <img src={cambio} className='icon-cambio' /> 
+                        <label htmlFor="cambio" className='lbl-cambio'> Cambio Manual</label>
+                        <br />
+                        <img src={porta} className='icon-porta' /> 
+                        <label htmlFor="number-porta" className='lbl-porta'> 4</label><label htmlFor="porta" className='lbl-porta'> Portas</label>
+                    </div>
+                    <div className='info-right'>
+                        <img src={small_bag} className='icon-bag'/>
+                        <label htmlFor="number-mala" className='lbl-mala'> 1</label><label htmlFor="mala" className='lbl-mala'> Bagagem Pequena</label>
+                        <br />
+                        <img src={large_bag} className='icon-bag'/>
+                        <label htmlFor="number-mala" className='lbl-mala'> 1</label><label htmlFor="mala" className='lbl-mala'> Bagagem Grande</label>
+                    </div>
+                </div>
+                <hr color='D4D4D4'/>
+                <div class="card-price">
+                    <div className="info-preco">
+                        <div className="preco">
+                            <label>A partir de:</label>
+                            <span className="preco-carro">R$ 146</span>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className='sub-car'>
-            <label htmlFor="local" className='lbl-local'>São Paulo - Juscelino Kubitscheck</label>
-            </div>
-            <div className='info-left'>
-                <img src={people} className='icon-assento' /> 
-                <label htmlFor="number-assento" className='lbl-assento'> 5</label><label htmlFor="assento" className='lbl-assento'> Assentos</label>
-                <br />
-                <img src={cambio} className='icon-cambio' /> 
-                <label htmlFor="cambio" className='lbl-cambio'> Cambio Manual</label>
-                <br />
-                <img src={porta} className='icon-porta' /> 
-                <label htmlFor="number-porta" className='lbl-porta'> 4</label><label htmlFor="porta" className='lbl-porta'> Portas</label>
-            </div>
-            <div className='info-right'>
-            <img src={small_bag} className='icon-bag'/>
-            <label htmlFor="number-mala" className='lbl-mala'> 1</label><label htmlFor="mala" className='lbl-mala'> Bagagem Pequena</label>
-            <br />
-            <img src={large_bag} className='icon-bag'/>
-            <label htmlFor="number-mala" className='lbl-mala'> 1</label><label htmlFor="mala" className='lbl-mala'> Bagagem Grande</label>
-            </div>
-
-
-        </div>
-            <hr color='D4D4D4'/>
-        <div class="card-price">
-            <p><strong>Preço:</strong></p>
-            <p class="price-value">R$ 150,00</p>
-            <p class="price-period">por diária</p>
-        </div>
-    </div>
-
         </>
     )
 
