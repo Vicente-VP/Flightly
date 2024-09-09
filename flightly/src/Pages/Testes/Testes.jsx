c81b0645d03e218fdcc33d7dab1f27623cfa88c3
Breno Saraiva <brenosaraiva27s@gmail.com>
2024-09-09 10:26:02 -0300

    Adição da página de Testes

---
+++ flightly/src/Pages/Testes/Testes.jsx
@@ -0,0 +1,22 @@
+// ARQUIVO DEDICADO PARA TESTE DE COMPONENTES
+
+import NavBar from "../../Componentes/NavBar/NavBar";
+import Footer from "../../Componentes/Footer/Footer";
+import CardInfoVoo from "../../Componentes/Card_Informacoes/Voos/CardInfoVoo";
+
+import './style_Testes.css';
+import React, { useState } from 'react';
+
+export default function Home() {
+    return (
+        <>
+            <div className="navbar-testes"><NavBar/></div>
+            
+            <div className="conteudo-testes">
+                <CardInfoVoo/>
+            </div>
+
+            <div className="footer-testes"><Footer/></div>
+        </>
+    )
+}
