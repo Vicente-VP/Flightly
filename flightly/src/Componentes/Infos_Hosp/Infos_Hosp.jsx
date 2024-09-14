29862e098bb2fdfaffa4dede3e1443ba9d6f4e4c
AGATA CECILIA <ceciliacesquim@gmail.com>
2024-09-14 18:31:52 -0300

    Infos Hosp com props

--- flightly/src/Componentes/Infos_Hosp/Infos_Hosp.jsx
+++ flightly/src/Componentes/Infos_Hosp/Infos_Hosp.jsx
@@ -1,37 +1,36 @@
-import ImgHosp from '../../Images/Infos_Hosp/Img_Hosp.png';
-import Star_Info_Hosp from '../../Images/Infos_Hosp/Star_Info_Hosp.png';
-import Wifi_Info_Hosp from '../../Images/Infos_Hosp/Wifi_Info_Hosp.png';
-import Refeicao_Info_Hosp from '../../Images/Infos_Hosp/Refeicao_Info_Hosp.png';
-import Cafe_Info_Hosp from '../../Images/Infos_Hosp/Cafe_Info_Hosp.png';
-import Psicina_Info_Hosp from '../../Images/Infos_Hosp/Pscina_Info_Hosp.png';
 import './Infos_Hosp.css';
 
-export default function InfoHosp(){
+export default function InfoHosp(props){
     return(
         <>
             <div className="container-Card">
 
                 <div className="left">
-                    <img src={ImgHosp} alt="ImgHosp" className='imghosp'/>
+                    <img src={props.image} alt={props.hotelName} className='imghosp'/>
                     <div className="info">
-                        <label className='titleinfohosp'>Hotel Atlântico Business</label>
-                        <span className='localinfohosp'>São Paulo, Peruíbe</span>
-                        <div>
-                            <img src={Star_Info_Hosp} alt="Star_Info_Hosp" className='starinfohosp'/>
-                            <span className='numstarinfohosp'>4.0</span>
-                            <span className='numvotinfohosp'>(9000)</span>
+                        <label className='titleinfohosp'>{props.hotelName}</label>
+                        <span className='localinfohosp'>{props.location}</span>
+                        <div className='star-rating'>
+                            <div className='starinfohosp'/>
+                            <span className='numstarinfohosp'>{props.rating}</span>
+                            <span className='numvotinfohosp'>({props.votes})</span>
                         </div>
                         <div>
-                            <span className='descinfohosp'>Situado em uma localização privilegiada, a poucos passos das deslumbrantes praias de areia dourada e águas cristalinas, o Hotel Sol Nascente oferece a combinação perfeita de conforto e conveniência.</span>
+                            <span className='descinfohosp'>{props.description}</span>
                         </div>
                         <div>
                             <label className='localinfohosp'>Inclui:</label>
                         </div>
                         <div className='services'>
-                            <div className='cardwifiinfohosp'>Wi-fi Grátis</div>
-                            <div className='cardrefeicaoinfohosp'>Restaurante</div>
-                            <div className='cardcafeinfohosp'>Café da Manhã</div>
-                            <div className='cardpsicinainfohosp'>Pscina</div>
+                            {props.services && props.services.length > 0 ? (
+                            props.services.map((service, index) => (
+                            <div key={index} className={`card${service.className}`}>
+                                {service.name}
+                            </div>
+                            ))
+                            ) : (
+                            <div>Nenhum serviço disponível</div>
+                        )}
                         </div>
                     </div>
                 </div>
@@ -41,7 +40,7 @@ export default function InfoHosp(){
                     <div className="info-preco">
                         <div className="preco">
                             <label className='titleprecoinfohosp'>A partir de:</label>
-                            <span className="precoinfohosp">R$ 1.678</span>
+                            <span className="precoinfohosp">{props.price}</span>
                         </div>
                     </div>
                 </div>
